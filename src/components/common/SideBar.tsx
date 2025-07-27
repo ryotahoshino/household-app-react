81e1fa0bf6ae3f76f3f91ec01dee54843bec94bb
ryotahoshino <ryota240833@gmail.com>
2025-07-27 12:13:33 +0900

    Interfaceの更新

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -1,7 +1,21 @@
 import { Box, Drawer, Toolbar, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import React from 'react';
-import InboxIcon from '@mui/icons-material/Inbox';
-import MailIcon from '@mui/icons-material/Mail';
+import HomeIcon from '@mui/icons-material/Home';
+import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt';
+
+interface SideBarProps {
+  drawerWidth: number;
+  mobileOpen: boolean;
+  handleDrawerToggle: () => void;
+  handleDrawerClose: () => void;
+  handleDrawerTransitionEnd: () => void;
+}
+
+interface menuItem {
+  text: string;
+  path: string;
+  icon: React.ComponentType,
+}
 
 const SideBar = ({
   drawerWidth,
@@ -9,38 +23,26 @@ const SideBar = ({
   handleDrawerToggle,
   handleDrawerClose,
   handleDrawerTransitionEnd,
-}: {
-  drawerWidth: number,
-  mobileOpen: boolean,
-  handleDrawerToggle: () => void,
-  handleDrawerClose: () => void,
-  handleDrawerTransitionEnd: () => void
-}) => {
+}: SideBarProps) => {
+
+  const MenuItems: menuItem[] = [
+    { text: 'Home', path: '/', icon: HomeIcon },
+    { text: 'Report', path: '/report', icon: SignalCellularAltIcon },
+  ]
+
   const drawer = (
     <div>
       <Toolbar />
       <Divider />
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
+        {MenuItems.map((item, index) => (
+          <ListItem key={index} disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                {/*index % 2 === 0 ? <InboxIcon /> : <MailIcon />*/}
+                <item.icon />
               </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemText primary={item.text} />
             </ListItemButton>
           </ListItem>
         ))}
